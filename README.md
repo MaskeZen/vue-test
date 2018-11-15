fc3b8c9166e984567749c68248207aeca2eef6b9
Diego Mascheroni <maskeee@gmail.com>
2018-11-15 00:44:43 -0300

    10 - eventos

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pruebas con vue
+
+## Laracast
+
+- [10 - eventos](https://laracasts.com/series/learn-vue-2-step-by-step/episodes/10)
--- main.js
+++ main.js
@@ -27,8 +27,54 @@ Vue.component('task', {
     }
 });
 
+Vue.component('message', {
+    template: `
+        <article class="message" v-show="isVisible">
+            <div class="message-header">
+            <p>{{ title }}</p>
+            <button class="delete" aria-label="delete"
+                @click="hideModal" ></button>
+            </div>
+            <div class="message-body">
+                {{ body }}
+            </div>
+        </article>
+    `,
+    props: ['title', 'body'],
+    data () {
+        return {
+            isVisible: true
+        }
+    },
+    methods: {
+        hideModal() {
+            this.isVisible = false
+        }
+    }
+});
+
+// 10 - Eventos (modal)
+Vue.component('modal', {
+    template: `
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="box">
+                <slot></slot>
+            </div>
+        </div>
+        <button class="modal-close is-large" aria-label="close"
+            @click="$emit('close')">
+        </button>
+    </div>`
+})
+
+// 11 - Tabs
 
 
 new Vue({
-    el: '#root'
+    el: '#root',
+    data: {
+        showModal: false
+    }
 });
